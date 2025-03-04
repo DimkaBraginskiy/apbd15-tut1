6ddc5d9bba8bf144a2a46b378963482836fc44ef
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-03-04 21:40:51 +0100

    average calculation implementation

--- APBD15-TUT1/APBD15-TUT1/Program.cs
+++ APBD15-TUT1/APBD15-TUT1/Program.cs
@@ -1,8 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Security.AccessControl;
 
-Console.WriteLine("Modification 1");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Modification 2");
+        Console.WriteLine("Modification 1");
 
-Console.WriteLine("Modification 3");
+        Console.WriteLine("Modification 2");
+
+        Console.WriteLine("Modification 3");
+
+        double[] arr = { 1, 2, 3.5, 4, 5 };
+
+        Console.WriteLine(GetAverage(arr));
+    }
+    
+    static double GetAverage(double[] array)
+    {
+        double sum = 0;
+        int count = array.Length;
+        foreach(double num in array)
+        {
+            sum += num;
+        }
+        return sum/count;
+    }
+}
+
+
+
